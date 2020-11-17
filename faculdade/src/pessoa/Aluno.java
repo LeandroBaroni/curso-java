526e588a5b12a666da038e604fa19af146cd21ac
Leandro Baroni <leandrobaroni2103@hotmail.com>
2020-11-17 04:23:28 -0300

    Construtores em Classe Aluno

--- faculdade/src/pessoa/Aluno.java
+++ faculdade/src/pessoa/Aluno.java
@@ -3,4 +3,13 @@ package pessoa;
 public class Aluno {
 	private int id;
 	private String nome;
+
+	public Aluno(){
+	}
+
+	
+	public Aluno(int id, String nome){
+		this.id = id;
+		this.nome = nome;
+	}
 }
